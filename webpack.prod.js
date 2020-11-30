d1cb442e60a949c37e25a95d25e96fba51898537
zhengsubei <786942047@qq.com>
2020-11-30 19:55:48 +0800

    feat: split

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const {merge} = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common,{
+    mode: 'production',
+    plugins: [
+        new UglifyJsPlugin()
+    ]
+})
